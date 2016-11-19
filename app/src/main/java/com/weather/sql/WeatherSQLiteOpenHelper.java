b2b7f3e7708105e3da80c89483022b7d7254dd21
YuanStudio <Sijiamomo@126.com>
2016-11-19 19:25:34 +0800

    Initial commit

---
+++ app/src/main/java/com/weather/sql/WeatherSQLiteOpenHelper.java
@@ -0,0 +1,21 @@
+package com.weather.sql;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class WeatherSQLiteOpenHelper extends SQLiteOpenHelper {
+    public WeatherSQLiteOpenHelper(Context context){
+        super(context,"weather",null,0);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table cityweather(id integer primary key autoincrement,cityName varchar,cityId integer)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
